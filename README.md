bc00227dc5c83f6cdd5814355b767e5f61e58f42
ryamad <ry0922ry1@gmail.com>
2021-07-23 19:35:50 +0900

    No github actions

--- README.md
+++ README.md
@@ -4,8 +4,7 @@ Papers_with_Code_bot is a bot for notifying Slack of SOTA updates on the web usi
 
 ## Descriptions
 
-[Browse State-of-the-Art](https://paperswithcode.com/sota) is a very informative website where SOTA for machine learning is summarized. Papers_with_Code_bot checks the SOTA updates of some tasks on the website every day and notifies Slack using Github Actions when they are updated. The notifications include the model, the paper that proposed it, and its URL before and after the SOTA update.
-
+[Browse State-of-the-Art](https://paperswithcode.com/sota) is a very informative website where SOTA for machine learning is summarized. Papers_with_Code_bot checks the SOTA updates of some tasks on the website every day and notifies Slack or some messaging apps when they are updated. The notifications include the model, the paper that proposed it, and its URL before and after the SOTA update.
 
 ## Requirements
 
@@ -17,34 +16,25 @@ Papers_with_Code_bot is a bot for notifying Slack of SOTA updates on the web usi
 - pandas 1.1.5
 - PyYAML 3.13
 - requests 2.25.1
+- schedule 1.1.0
 
 ## Usage
+
+```
+$ python main.py --webhook_url [your_incoming_webhook_url] --needsInitialCsv [bool]
+```
+
+- Argument:
+  - webhook_url: (str) Your incoming webhook url
+  - needsInitialCsv: (bool) Whether you need initial csv files or not (for debug)
+
+If csv files don't exist when the code is executed, they will be created but no message will be sent to Slack or some messaging apps.
+
+### How to choose the tasks
 
 You can choose any kind of task on [Browse State-of-the-Art](https://paperswithcode.com/sota). You should check the URL for each task and set the last part of it (as shown in the red dotted frame below) as `tasks` in `config.yaml`. (the defaults are `['image-classification', 'object-detection']`)
 
 ![last_part_of_url](last_part_of_url.png)
-
-If csv files don't exist when the code is executed, they will be created but no message will be sent to Slack.
-### GitHub Actions
-
-Set as follows:
-
-- your incoming webhook url as an environment variable in your repository (required)
-- Slack notification time in `.github/workflow/cron.yml` (optional, the default is midnight UTC)
-
-```
-$ python main.py
-```
-
-### Debug
-
-```
-$ python main.py --slack_id [your_incoming_webhook_url] --needsInitialCsv [bool]
-```
-
-- Argument: 
-    - slack_id: (str) Your incoming webhook url
-    - needsInitialCsv: (bool) Whether you need initial csv files or not
 
 ## License
 
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import argparse
 from distutils.util import strtobool
+import schedule
 
 
 def get_atag(data):
@@ -35,7 +36,7 @@ def create_inicsv(task, filepath):
     df.to_csv(filepath, index=False)
 
 
-def make_msg(soup, slack_id, filepath):
+def make_msg(soup, webhook_url, filepath):
     df = pd.read_csv(filepath, index_col=0)
     for dataset, model, paper, url in zip(
         soup.select("[class='dataset black-links']"),
@@ -59,19 +60,20 @@ def make_msg(soup, slack_id, filepath):
                 paperurl,
             ]
             text = "\n".join(lst_text)
-            requests.post(
-                slack_id,
-                data=json.dumps(
-                    {"text": text},
-                ),
-            )
+            if webhook_url is not None:
+                requests.post(
+                    webhook_url,
+                    data=json.dumps(
+                        {"text": text},
+                    ),
+                )
             time.sleep(1)
             df.loc[dataset].iloc[0] = model
             df.loc[dataset].iloc[1] = paper
     df.to_csv(filepath)
 
 
-def check_update(slack_id: str, lst_tasks: list, needsInitialCsv: int) -> None:
+def check_update(webhook_url: str, lst_tasks: list, needsInitialCsv: int) -> None:
     for task in lst_tasks:
         soup = get_request(task)
         filepath = os.path.join(os.getcwd(), task + ".csv")
@@ -80,21 +82,33 @@ def check_update(slack_id: str, lst_tasks: list, needsInitialCsv: int) -> None:
             create_inicsv(task, filepath)
         else:
             print(f"Making Slack msg of {task}")
-            make_msg(soup, slack_id, filepath)
+            make_msg(soup, webhook_url, filepath)
         time.sleep(1)
 
 
 def main():
-    parser = argparse.ArgumentParser(description="slack bot")
-    parser.add_argument("--slack_id", type=str, help="incoming webhook url")
-    parser.add_argument("--needsInitialCsv", type=strtobool, default=False, help="initial file")
+    parser = argparse.ArgumentParser(description="bot")
+    parser.add_argument("--webhook_url", type=str, help="incoming webhook url")
+    parser.add_argument(
+        "--needsInitialCsv", type=strtobool, default=False, help="initial file"
+    )
     args = parser.parse_args()
 
-    slack_id = os.getenv("SLACK_ID") or args.slack_id
     with open(os.path.join(os.getcwd(), "config.yaml")) as file:
         obj = yaml.safe_load(file)
     print(obj)
-    check_update(slack_id, obj["tasks"], args.needsInitialCsv)
+
+    # every day
+    schedule.every().day.at(obj["time"]).do(
+        check_update,
+        webhook_url=args.webhook_url,
+        lst_tasks=obj["tasks"],
+        needsInitialCsv=args.needsInitialCsv,
+    )
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 
 if __name__ == "__main__":
